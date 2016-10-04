ddd7985f62476a1d146cf10cee0d4bd5e8778429
Cristi Draghici <cristi@draghici.net>
2016-10-04 22:26:12 +0300

    Added support for pgsql

--- README.md
+++ README.md
@@ -1,27 +1,23 @@
-# sync-mysql
+# sync-sql
 
-**sync-mysql** is designed to make synchronous queries to the database. It has been developed as a tool to help [nightwatch.js](http://nightwatchjs.org/) tests - or other such tools - connect directly to a mysql database.
-
-## Testing
-
-**This package is still undergoing tests.**
+**sync-sql** is designed to make synchronous queries to the database. It has been developed as a tool to help [nightwatch.js](http://nightwatchjs.org/) tests - or other such tools - connect directly to a mysql database.
 
 ## Important note
 
-You should not be using this in a production evironment. Node.js is designed to be asynchronous, therefore running sync queries on the database will seriously impact performance and stability.
+**!!!** You should **not be using** this in a production evironment. Node.js is designed to be asynchronous, therefore running sync queries on the database will seriously impact performance and stability.
 
 ## Install
 
 ```
-npm install cristidraghici/sync-mysql
+npm install cristidraghici/sync-sql
 ```
 
-## Usage
+## Example usage with mysql
 
 ```js
-var syncMysql = require('sync-mysql');
+var syncSql = require('sync-sql');
 
-var output = syncMysql(
+var output = syncSql.mysql(
 	{
 		host: 'localhost',
 		user: 'user',
@@ -39,13 +35,15 @@ console.log(JSON.stringify(output));
 
 - please edit the *.env.example* with database information and the sql to test and save the file as *.env*;
 - run `npm install`;
+- run `npm run test-mysql`;
+- run `npm run test-pg`;
 - run `npm run test`.
 
 ## Workflow
 
 Internally, this uses a separate worker process that is run using [childProcess.spawnSync](http://nodejs.org/docs/v0.11.13/api/child_process.html#child_process_child_process_spawnsync_command_args_options).
 
-The worker then makes the actual query using the [mysql](https://www.npmjs.com/package/mysql) package.
+The worker then makes the actual query using the [mysql](https://www.npmjs.com/package/mysql) and [pg](https://www.npmjs.com/package/pg) packages.
 
 ## Thanks
 
--- index.js
+++ index.js
@@ -6,13 +6,13 @@ var JSONB = require('json-buffer');
 var fs = require('fs');
 
 /**
- * Execute the query on the database
- * @throws {Error} Errors thrown while executing
+ * Execute the query on the mysql database
+ * @throws  {Error}  Errors thrown while executing
  * @param   {string} query      The query to execute
  * @param   {object} connection Connection information: host, user, pass, database, port (optional)
  * @returns {object} Object containing the response
  */
-module.exports = function (connection, query, params) {
+module.exports.mysql = function (connection, query, params) {
 	var req,
 		res;
 	
@@ -21,13 +21,8 @@ module.exports = function (connection, query, params) {
 	}
 
 	req = JSONB.stringify({
-		connection: {
-			host: connection.host,
-			user: connection.user,
-			password: connection.password,
-			database: connection.database,
-			port: parseInt(connection.port, 10)
-		},
+        type: 'mysql',
+		connection: connection,
 		query: query || '',
 		params: params || []
 	});
@@ -52,3 +47,48 @@ module.exports = function (connection, query, params) {
 	// get the response
 	return JSONB.parse(res.stdout);
 };
+
+/**
+ * Execute the query on the postgres database
+ * @throws  {Error}  Errors thrown while executing
+ * @param   {string} query      The query to execute
+ * @param   {object} connection Connection information: host, user, pass, database, port (optional)
+ * @returns {object} Object containing the response
+ */
+module.exports.pgsql = function (connection, query, params) {
+	var req,
+		res;
+	
+	if (!spawnSync) {
+		throw new Error('Node version must be >= 0.12');
+	}
+
+	req = JSONB.stringify({
+        type: 'postgres',
+		connection: connection,
+		query: query || '',
+		params: params || []
+	});
+
+	res = spawnSync(process.execPath, [require.resolve('./lib/worker.js')], {
+		input: req
+	});
+
+	// Error handling on the process itself
+	if (res.status !== 0) {
+		throw new Error(res.stderr.toString());
+	}
+
+	if (res.error) {
+		if (typeof res.error === 'string') {
+			res.error = new Error(res.error);
+		}
+
+		throw res.error;
+	}
+
+	// get the response
+	return JSONB.parse(res.stdout);
+};
+// Postgres aliases
+module.exports.pg = module.exports.postgres = module.exports.pgsql;
